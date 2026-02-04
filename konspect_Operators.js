9da60eff6a66e2ff2f8470cd0a30459c1481f9a2
Денис Шрамков <denis@MacBook-Air-Denis.local>
2026-02-04 21:05:18 +0200

    more info about logical operators, added konspekt about if, added initial konspekt about iterators

--- konspect_Operators.js
+++ konspect_Operators.js
@@ -21,3 +21,10 @@ const isChecked = true,
 
 console.log(isChecked && isClosed) // false
 console.log(isChecked || isClosed) // true, because one of them is true
+
+console.log(1 && 0); // 0, because 1 is true, but 0 is false, It'll show the value where it stopped
+console.log(0 && 1); // 0, because 0 is false, it'll show the first false value
+console.log(1 && 5); // 5, because 1 is true, and 5 is true, it'll show the last value
+console.log(null || 2 || undefined); // 2, because null and undefined are false, but 2 is true, it'll show the first true value
+console.log(1 || 0); // 1, because 1 is true, it'll show the first true value	
+// or || stops at the first true value, and and && stops at the first false value
